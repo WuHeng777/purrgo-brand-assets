e46bf612a31a6cd9f1965f98be1b29149ce89067
football66 <86511435+WuHeng777@users.noreply.github.com>
2025-09-02 18:34:48 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'purrgo-v14';
+const CACHE = 'purrgo-v15';
 const ASSETS = [
   '/', './index.html', './style.css', './manifest.webmanifest',
   './data/products.json', './data/locations.json', './data/hospitals.json', './data/breeds.json',
