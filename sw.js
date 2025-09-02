2672a6a34dacc95d518764e288729df6cd3ff96b
football66 <86511435+WuHeng777@users.noreply.github.com>
2025-09-02 21:04:30 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,36 @@
+// 超精簡：確保相對路徑在 GitHub Pages 正常
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => self.clients.claim());
 
+// 只做最基本的快取優先 (可依需要擴充)
+const CACHE = 'purrgo-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './data/products.json',
+  './data/locations.json',
+  './data/breeds.json',
+  './data/hospitals.json',
+  './logo/logo-192.png',
+  './logo/logo-512.png',
+  './logo/favicon.png'
+];
+
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));
+});
+
+self.addEventListener('fetch', e=>{
+  const req = e.request;
+  e.respondWith(
+    caches.match(req).then(hit => hit || fetch(req).then(res=>{
+      // 靜態資源放入快取
+      if (req.url.startsWith(self.location.origin)) {
+        const clone = res.clone();
+        caches.open(CACHE).then(c=>c.put(req, clone));
+      }
+      return res;
+    }).catch(()=>hit))
+  );
+});
