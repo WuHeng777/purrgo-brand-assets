63204728c30dfed3ca37193053264e903b8940bd
football66 <86511435+WuHeng777@users.noreply.github.com>
2025-09-08 20:40:16 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* PurrGo Service Worker — clean single version */
-const CACHE = 'purrgo-v21'; // ← 每次前端改版請改這個字串
+const CACHE = 'purrgo-v22'; // ← 每次前端改版請改這個字串
 const ASSETS = [
   './',
   './index.html',
